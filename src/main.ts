53f5fb05521e4eec08ca9f0b44d4ac0a9ddc87af
alkanaziz <alkanaziz@gmail.com>
2023-12-01 11:08:34 +0100

    strings-numbers finished

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import "./style.scss";
 
+const firstName = "George";
+const age = 44;
+
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = /*html*/`
 <h1 class="text-3xl mb-3">Vitest Site</h1>
-<p>testing</p>
+<p>${firstName} is ${age}</p>
+<p>Next year, ${firstName} will be ${age+1}</p>
+<p>The variable firstName is of type ${typeof firstName}</p>
+<p>The variable age is of type ${typeof age}</p>
 `;
